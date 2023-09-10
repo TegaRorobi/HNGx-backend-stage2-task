d8f1fdd5c2c54befd372b9b3d3f74a27aa5c5c9b
Tega Rorobi <rorobitega@gmail.com>
2023-09-10 22:59:16 +0100

    Creted a 'Person' model and ran migrations

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# The resource we'll perform the CRUD operations on
+class Person(models.Model):
+	'''
+	All fields contain only string datatypes, including
+	the new primary key field 'name' instead of 'id'
+	'''
+
+	name = models.CharField(max_length=100, primary_key=True, unique=True)
+	email = models.EmailField()
+	bio = models.TextField(null=True, blank=True)
+
+	def __str__(self):
+		return self.name

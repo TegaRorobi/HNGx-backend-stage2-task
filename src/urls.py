a40ab8db2819f92868462cecc5d13863fea4e42f
Tega Rorobi <rorobitega@gmail.com>
2023-09-11 06:54:04 +0100

    Changed the endpoint urls to /api/person/..../

--- src/urls.py
+++ src/urls.py
@@ -24,6 +24,6 @@ RETRIEVE_UPDATE_DESTROY = {'get':'retrieve', 'put':'update', 'patch':'partial_up
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', PersonsViewSet.as_view(LIST_CREATE), name='persons-list'),
-    path('api/<str:pk>/', PersonsViewSet.as_view(RETRIEVE_UPDATE_DESTROY), name='person-detail'),
+    path('api/persons/', PersonsViewSet.as_view(LIST_CREATE), name='persons-list'),
+    path('api/persons/<str:pk>/', PersonsViewSet.as_view(RETRIEVE_UPDATE_DESTROY), name='person-detail'),
 ]
